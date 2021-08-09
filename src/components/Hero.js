7d759cedbfe25b22d9c16c6ddad114add830c355
Benedikt Schnupp <benediktschnupp@me.com>
2021-08-09 23:00:36 +0200

    mobile menu

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from 'gatsby'
 import BackgroundImage from 'gatsby-background-image'
 
@@ -6,17 +6,27 @@ import Logo from '../components/Logo'
 import Button from '../components/Button'
 
 import meta from "../../content/settings/global.yml"
+import menu from "../assets/menu.svg"
+import close from "../assets/x.svg"
 
 
 function Hero(props) {
+    const [toggle, setToggle] = useState(false)
+    console.log(toggle);
     var hero = props.hero
-    console.log(props);
     return(
     <div className={"hero " + props.variant}>
         <div className="layout">
             <div className="grid">
                 <header>
                     <Logo meta={meta} />
+                    <div className="hamburger">
+                        {toggle === false ?
+                            <a role="menu" onClick={()=> setToggle(!toggle)}><img src={menu} /></a>
+                        :
+                            <a role="menu" onClick={()=> setToggle(!toggle)}><img src={close} /></a>
+                        }
+                    </div>
                     <nav>
                         {meta.menu.map((item, i)=> <Link className="navbar-item" to={item.to} key={"menu-"+i}>{item.name}</Link>)}
                     </nav>
@@ -57,6 +67,12 @@ function Hero(props) {
             : props.imageslider ? props.imageslider : null}
             </div>
         </div>
+    </div>
+    <div className={toggle === true ? "mobilemenu visible" : "mobilemenu"}>
+        <main>
+            {meta.menu.map((item, i)=> <Link className="navbar-item" to={item.to} key={"menu-"+i}>{item.name}</Link>)}
+            {meta.footermenu.map((item, i)=> <Link className="navbar-item" to={item.to} key={"menu-"+i}>{item.name}</Link>)}
+        </main>
     </div>
 </div>
     )
