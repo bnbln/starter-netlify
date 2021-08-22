01cd8b7f00c1293177f4dfc819689a86852b63a0
Benedikt Schnupp <benediktschnupp@me.com>
2021-08-23 01:03:21 +0200

    fix icon

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
 import Layout from '../../components/Layout'
-import icon from "../../assets/message-circle.svg"
+
+const Thanks = (props) => (
+  <Layout data={{title: "Danke", lead: "Ihre Nachricht wurde versendet. Wir werden uns zeitnah mit Ihnen in Verbindung setzen.", icon: props.data.markdownRemark.frontmatter.icon}}>
+  </Layout>
+)
 
 export default () => (
-  <Layout data={{title: "Danke", lead: "Ihre Nachricht wurde versendet. Wir werden uns zeitnah mit Ihnen in Verbindung setzen.", icon: icon}}>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query contactThanksQuery {
+        markdownRemark(frontmatter: {templateKey: {eq: "contact-page"}}) {
+          frontmatter {
+            title
+            templateKey
+            lead
+            icon {
+              publicURL
+              extension
+              childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            image {
+              childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <Thanks data={data} count={count} />}
+  />
 )
